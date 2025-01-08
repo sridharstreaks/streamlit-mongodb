543d55ef03867967288b2a0ba56b189df44c3e07
Sridhar Streaks <100800059+sridharstreaks@users.noreply.github.com>
2025-01-08 21:29:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,11 +61,11 @@ def monitor_and_stream_video():
     else:
         st.video(video_path)
         s = handle.status()
-        st.write(s.progress)
-        progress_placeholder.write(
-            f"Progress: {s.progress * 100:.2f}% (down: {s.download_rate / 1000:.1f} kB/s, "
-            f"seeds: {s.num_seeds}, peers: {s.num_peers})"
-        )
+        while s.progress<1:
+            progress_placeholder.write(
+                f"Progress: {s.progress * 100:.2f}% (down: {s.download_rate / 1000:.1f} kB/s, "
+                f"seeds: {s.num_seeds}, peers: {s.num_peers})"
+            )
 
 # Streamlit UI
 st.title("Stream Torrent Video")
